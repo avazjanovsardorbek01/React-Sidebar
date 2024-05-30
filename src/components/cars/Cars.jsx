cfb0e4c14e336d69ae3805d2153e4970f60a1435
avazjanovsardorbek01 <sardoravazzanov@gmail.com>
2024-05-30 13:15:48 +1000

    add-modal

--- src/components/cars/Cars.jsx
+++ src/components/cars/Cars.jsx
@@ -1,7 +1,74 @@
+import { useState } from "react";
+import UserModal from "../Modal";
 import "./cars.css";
-
 const Cars = () => {
-  return <div>Cars</div>;
+  const [cars, setCars] = useState([]);
+  const [modal, setModal] = useState(false);
+  const openModal = () => {
+    setModal(true);
+  };
+  return (
+    <>
+      <UserModal open={modal} toogle={() => setModal(false)} />
+      <div className="container">
+        <div className="row mt-3">
+          <div className="col-md-10 offset-1">
+            <div className="row">
+              <div className="col-4">
+                <button className="btn btn-success" onClick={openModal}>
+                  Add user
+                </button>
+              </div>
+              <div className="col-8">
+                <input
+                  type="text"
+                  placeholder="Search..."
+                  className="form-control"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="row mt-3">
+          <table>
+            <thead>
+              <tr>
+                <td>T/R</td>
+                <td>Name</td>
+                <td>Price</td>
+                <td>Year</td>
+                <td>Color</td>
+                <td>Brand</td>
+                <td>Action</td>
+              </tr>
+            </thead>
+            <tbody>
+              {cars.map((item, inedx) => (
+                <tr key={index}>
+                  <td>{index + 1}</td>
+                  <td>{index.name}</td>
+                  <td>{index + price}</td>
+                  <td>{index + year}</td>
+                  <td>{index + color}</td>
+                  <td>{index + brand}</td>
+                  <td>
+                    <div className="d-flex gap-2">
+                      <button className="btn btn-info">
+                        <i className="fa-solid fa-pen-to-square"></i>
+                      </button>
+                      <button className="btn btn-danger">
+                        <i className="fa-solid fa-trash-can"></i>{" "}
+                      </button>
+                    </div>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Cars;
